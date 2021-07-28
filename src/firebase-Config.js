63ed91fbe7eb3415f28fffa194cc79fc6a69429b
Rishav-Chanda <your_email_address@example.com>
2021-07-28 16:27:33 +0530

    .

---
+++ src/firebase-Config.js
@@ -0,0 +1,15 @@
+import firebase from "firebase";
+var firebaseConfig = {
+    apiKey: "AIzaSyDedtmNx-cfWiyleVcb9Sm9x8R77fGUZoo",
+    authDomain: "whatsapp-clone-rishav.firebaseapp.com",
+    projectId: "whatsapp-clone-rishav",
+    storageBucket: "whatsapp-clone-rishav.appspot.com",
+    messagingSenderId: "902572169035",
+    appId: "1:902572169035:web:a53034dd4cbfb3a6dc7ace",
+    measurementId: "G-87WVDGQBKE"
+  };
+  // Initialize Firebase
+  firebase.analytics();
+  firebase.initializeApp(firebaseConfig);
+  const db = firebase.firestore();
+  export { db };
